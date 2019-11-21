793adea900e52176c218eebd5d3a8e7acce2a7d6
ttse-sfdc <ttse@salesforce.com>
2019-11-21 10:58:42 -0800

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 14 -s -w 60
 sfdx force:source:push
 sfdx force:user:password:generate
-
+sfdx force:user:display
 sfdx force:org:open -p /analytics/wave/wave.apexp#home
